0d29d190abf7fef95eb8f62b6a3a198ff2ab8834
peterwilmshurst <peterwilmshurst@hotmail.com>
2023-07-22 11:46:39 +0100

    change button to toggle

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="app-container">
-    <button @click="toggleColor" style="position: absolute; top: 10px; right: 10px;">Toggle Color</button>
+    <label class="switch">
+      <input type="checkbox" @change="toggleColor">
+      <span class="slider round"></span>
+    </label>
     <RouterView />
   </div>
 </template>
@@ -76,5 +79,61 @@ html,
   justify-content: center;
   perspective: 1000px;
   background-color: v-bind(bgColor);
+}
+
+.switch {
+  position: absolute;
+  top: 10px; 
+  right: 10px;
+  width: 60px;
+  height: 34px;
+}
+
+.switch input { 
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  transform: translateX(26px);
+}
+
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
 }
 </style>
