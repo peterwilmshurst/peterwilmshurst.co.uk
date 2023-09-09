342bd48311acc933f97f1216ec3b1e0215453f4b
peterwilmshurst <peterwilmshurst@hotmail.com>
2023-09-09 20:40:54 +0100

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ["./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'brand-black': '#2A263D',
+        'brand-green': '#39b54a',
+        'brand-grey': '#38344d',
+      },
+    },
+  },
+  plugins: [],
+}
